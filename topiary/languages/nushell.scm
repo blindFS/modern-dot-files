57c401536726c859f276e309e3697423cd5b1373
blindFS <blindfs19@gmail.com>
2024-10-28 22:44:17 +0800

    topiary nushell comments

--- topiary/languages/nushell.scm
+++ topiary/languages/nushell.scm
@@ -31,6 +31,18 @@
   (comment)
 ] @prepend_space @append_space
 
+(expr_binary
+  lhs: _ @append_space
+  opr: _
+  rhs: _ @prepend_space
+)
+
+(assignment
+  lhs: _ @append_space
+  opr: _
+  rhs: _ @prepend_space
+)
+
 [
   "["
   "{"
@@ -46,12 +58,22 @@
 (decl_def
   "export"? @append_space
   (long_flag)? @prepend_space @append_space
-  quoted_name: (_)? @prepend_space @append_space
-  unquoted_name: (_)? @prepend_space @append_space
+  quoted_name: _? @prepend_space @append_space
+  unquoted_name: _? @prepend_space @append_space
   (parameter_bracks)?
   (returns)?
   (block) @prepend_space
 )
+
+(decl_use
+  module: _? @prepend_space @append_space
+  import_pattern: _? @prepend_space @append_space
+)
+
+[
+  (decl_def)
+  (decl_use)
+] @append_hardline
 
 (pipe_element
   "|" @prepend_space @append_space
@@ -59,7 +81,13 @@
 
 (ctrl_if
   "if" @append_space
-  condition: (_) @append_space
+  condition: _ @append_space
+)
+
+(ctrl_for
+  "for" @append_space
+  "in" @prepend_space @append_space
+  body: _ @prepend_space
 )
 
 (match_guard
@@ -68,50 +96,39 @@
 
 (ctrl_match
   "match" @append_space
-  scrutinee: (_)? @append_space
-  (match_arm) @prepend_spaced_softline
+  scrutinee: _? @append_space
+  (match_arm)? @prepend_spaced_softline
   (default_arm)? @prepend_spaced_softline
 )
 
 (list_body
-  entry: (_) @prepend_space @append_space @prepend_spaced_softline
+  entry: _ @append_space @prepend_spaced_softline
 )
 
 (val_list
-  item: (_) @prepend_space @append_space @prepend_spaced_softline
+  item: _ @append_space @prepend_spaced_softline
 )
 
 (record_body
-  entry: (record_entry) @prepend_space @append_space @prepend_spaced_softline
+  entry: (record_entry) @append_space @prepend_spaced_softline
 )
 
 (command
-  flag: (_)? @prepend_space
-  arg_str: (_)? @prepend_space
-  arg: (_)? @prepend_space
-)
-
-(decl_use
-  module: (_)? @prepend_space @append_space
-  import_pattern: (_)? @prepend_space @append_space
+  flag: _? @prepend_space
+  arg_str: _? @prepend_space
+  arg: _? @prepend_space
 )
 
 [
-  (decl_def)
-  (decl_use)
   "\n"
 ] @append_hardline
 
-[
-  (comment)
-] @append_empty_softline
+(
+  (comment) @append_empty_softline
+  .
+  "\n"? @do_nothing
+)
 
 [
   (parameter)
 ] @prepend_empty_softline
-
-(expr_binary
-  lhs: (_)? @append_space
-  opr: (_)?
-  rhs: (_)? @prepend_space
-)
