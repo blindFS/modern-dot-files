8fc389cf45cbcdab5b6efc9e0c3c554ad350586e
blindfs <blindfs19@gmail.com>
2026-02-19 19:37:39 +0800

    refactor(nix): ghostty config managed by home-manager settings

--- nix/modules/home/ghostty.nix
+++ nix/modules/home/ghostty.nix
@@ -7,17 +7,22 @@
   flake.homeModules.ghostty =
     { pkgs, ... }:
     {
-      # need to call zsh first in initial-command to load env variables set by nix
-      xdg.configFile."ghostty/config".text = ''
-        initial-command = zsh -l -c "${lib.getExe pkgs.tmux} new -A -s dev"
-        font-family = "${self.font.monofont}"
-        font-size = 15
-        theme = "${builtins.split "_" self.theme.colorscheme |> builtins.head}"
-        background-opacity = 0.8
-        background = #000000
-        macos-titlebar-style = hidden
-        custom-shader = shaders/cursor_blaze.glsl
-        quit-after-last-window-closed = true
-      '';
+      programs.ghostty = {
+        enable = true;
+        # ghostty installed by homebrew
+        package = null;
+        settings = {
+          # need to call zsh first in initial-command to load env variables set by nix
+          initial-command = "zsh -l -c \"${lib.getExe pkgs.tmux} new -A -s dev\"";
+          font-family = self.font.monofont;
+          font-size = 15;
+          theme = builtins.split "_" self.theme.colorscheme |> builtins.head;
+          background-opacity = 0.8;
+          background = "#000000";
+          macos-titlebar-style = "hidden";
+          custom-shader = "shaders/cursor_blaze.glsl";
+          quit-after-last-window-closed = true;
+        };
+      };
     };
 }
